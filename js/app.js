5a5252c9f32105c2175f1397c02bae96d937ec58
Arrick <Grapes01@gmail.com>
2019-03-21 07:44:42 -0700

    Commit 3 Excited Kittens Answered

--- js/app.js
+++ js/app.js
@@ -3,8 +3,19 @@ for(let i = 1; i <= 20; i++){
     console.log(i)
 }
 
-//Get Even
+// Get Even
 for(let i = 0; i <= 200; i+=2){
     if (i % 2 == 0); 
     console.log(i)
 }
+
+// Excited Kitten
+function getRandomInt(even) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  
+for(let i = 0; i < 20; i++){
+    
+    console.log('Love me, pet me! HSSSSSS!');
+}
+
