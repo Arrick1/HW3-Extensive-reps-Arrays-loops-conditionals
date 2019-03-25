7ae9ef0ea3936c9cfa9933757a74becd8e1400ac
Arrick <Grapes01@gmail.com>
2019-03-25 14:59:32 -0700

    Commit 3 Excited kittens answered

--- js/app.js
+++ js/app.js
@@ -10,12 +10,15 @@ for(let i = 0; i <= 200; i+=2){
 }
 
 // Excited Kitten
-function getRandomInt(even) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  
-for(let i = 0; i < 20; i++){
-    
-    console.log('Love me, pet me! HSSSSSS!');
-}
+// function getRandomInt(even) {
+//     return Math.floor(Math.random() * Math.floor(max));
+//   }
 
+var statements = ["...human...why you taking pictures of me?...", 
+"...the catnip made me do it...", "...why does the red dot always get away..."]
+for(let i = 0; i < 100; i++){
+if(i % 2 === 0) {
+    let index = Math.floor(Math.random()* statements.length)
+    console.log(statements[index]);
+}
+}
