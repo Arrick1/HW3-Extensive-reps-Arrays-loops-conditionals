2ce97ac79e93f2c9576e9b8556d19943e9a69b61
Arrick <Grapes01@gmail.com>
2019-03-26 16:52:54 -0700

    Commit 9 - Project Euler Problem 1 answered

--- js/app.js
+++ js/app.js
@@ -179,4 +179,27 @@ for(let i = 0; i<kristynsCloset.length; i++){
 // log the arrays (not the elements in the arrays) containing all of 
 //Thom's shirts, pants, and accessories.
 const inventory = thomsCloset[0][1][2];
-console.log(thomsCloset[0],thomsCloset[1], thomsCloset[2], thomsCloset[[3]]);
+console.log(thomsCloset[0],thomsCloset[1], thomsCloset[2], thomsCloset[3]);
+
+
+//multiples of 3 and 5
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+//we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+let sum = 0 
+for(i = 1; i < 1000; i++){
+    if((i % 5 === 0) || (i % 3 === 0)){
+      sum += i 
+}
+}
+console.log(sum);
+
+
+    // const sum = 
+    
+// var arguement = 7;
+
+//  while (arguement < 7){
+//      console.log("#");
+//  }
+//      console.log(arguement++)
